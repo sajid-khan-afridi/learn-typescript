7a24707a5d623856c875813479bacd2a03aefdf6
sajid-khan-afridi <69663016+sajid-khan-afridi@users.noreply.github.com>
2022-12-25 08:55:20 +0500

    Update app.ts

--- step00c_type_error/app.ts
+++ step00c_type_error/app.ts
@@ -1,3 +1,8 @@
 let message = "Hello World";
-console.loger(message);
-        
+// console.loger(message);
+
+let x: number = 10;
+let y: string = x;
+// let y: string = x.toString();
+
+console.log(y);
